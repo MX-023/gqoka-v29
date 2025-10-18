6c2f5fc3d3caea4408dde7791caf46194ffb294e
MX-023 <gqoka23@gmail.com>
2025-10-18 20:02:23 +0200

    Fix alias @ + stable imports + global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,11 @@
-import "@/styles/globals.css";
+// pages/_app.tsx
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Header from "../components/Header";
-import BotpressChat from "../components/BotpressChat";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen bg-black text-white antialiased">
-      <Header />
-      <main className="mx-auto w-full max-w-6xl px-5 pb-24 pt-8">
-        <Component {...pageProps} />
-      </main>
-      <BotpressChat />
-    </div>
+    <main className="min-h-screen bg-black text-white antialiased">
+      <Component {...pageProps} />
+    </main>
   );
 }
