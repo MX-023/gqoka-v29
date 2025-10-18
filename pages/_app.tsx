df90c8e4a7ca26d35f9aff2be53e64a1a7d05960
MX-023 <gqoka23@gmail.com>
2025-10-18 18:07:34 +0200

    Global UI dark mode + fonts + app wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,11 @@
 // pages/_app.tsx
-import type { AppProps } from "next/app";
 import "@/styles/globals.css";
-import Header from "@/components/Header";
-import BotpressChat from "@/components/BotpressChat";
-import { Inter, Playfair_Display } from "next/font/google";
+import type { AppProps } from "next/app";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-const playfair = Playfair_Display({
-  subsets: ["latin"],
-  variable: "--font-playfair",
-});
-
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${inter.variable} ${playfair.variable} bg-zinc-950 text-zinc-50 min-h-screen`}>
-      <Header />
-      <main className="mx-auto w-full max-w-6xl px-5 pb-24 pt-10">
-        <Component {...pageProps} />
-      </main>
-      <BotpressChat />
-    </div>
+    <main className="min-h-screen bg-black text-white antialiased">
+      <Component {...pageProps} />
+    </main>
   );
 }
